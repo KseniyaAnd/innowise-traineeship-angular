90c1c4cf34eec0a8503464d1f7dc1e540864ee66
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2025-01-16 11:51:34 +0300

    fix: save in form input limits

--- src/app/components/directives/limit-number-format.directive.ts
+++ src/app/components/directives/limit-number-format.directive.ts
@@ -1,24 +1,36 @@
-import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
   selector: '[appLimitNumberFormat]',
-  standalone: true
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => LimitNumberFormatDirective),
+      multi: true
+    }
+  ]
 })
-export class LimitNumberFormatDirective {
-  @Input () maxSize: number = Number.MAX_VALUE;
+export class LimitNumberFormatDirective implements ControlValueAccessor {
+  @Input() maxSize: number = Number.MAX_VALUE;
 
-  constructor(private el: ElementRef) {
-  }
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  constructor(private el: ElementRef) {}
 
   @HostListener('input', ['$event'])
   onInput(event: Event): void {
     const input = event.target as HTMLInputElement;
     let value: string = input.value;
 
-    if (parseFloat(value) > this.maxSize || parseFloat(value) < 0 ) {
+    if (parseFloat(value) > this.maxSize || parseFloat(value) < 0) {
       value = this.formatValue(value);
       input.value = value;
     }
+
+    this.onChange(value);
   }
 
   private formatValue(value: string): string {
@@ -33,4 +45,19 @@ export class LimitNumberFormatDirective {
     return numericValue.toString();
   }
 
+  writeValue(value: any): void {
+    this.el.nativeElement.value = value || '';
+  }
+
+  registerOnChange(fn: (value: any) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.el.nativeElement.disabled = isDisabled;
+  }
 }
