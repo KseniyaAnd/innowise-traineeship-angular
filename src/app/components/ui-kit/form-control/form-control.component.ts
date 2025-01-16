caf48a78a3ee09de615494b1fd0c7a5ef1259126
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2025-01-16 13:17:59 +0300

    add: mark required field

--- src/app/components/ui-kit/form-control/form-control.component.ts
+++ src/app/components/ui-kit/form-control/form-control.component.ts
@@ -1,13 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {booleanAttribute, Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-form-control',
   imports: [],
   templateUrl: './form-control.component.html',
-  styleUrl: './form-control.component.css',
   standalone: true
 })
 export class FormControlComponent {
   @Input () label: string
+  @Input ({transform: booleanAttribute}) required: boolean
     // NgControl
 }
